04fb55831fb01931028d8b490cd13f691acf8ff5
Gilbert <taya87136@.gmail.com>
2021-06-28 13:09:40 +0800

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by Hui Chih Wang on 2021/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
