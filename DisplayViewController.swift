149a01d2bbcf7bbb5a913b4facafdda413ca9b69
Gilbert <taya87136@.gmail.com>
2021-06-28 22:49:35 +0800

    add display view controller

---
+++ DisplayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DisplayViewController.swift
+//  CameraDemo
+//
+//  Created by Hui Chih Wang on 2021/6/28.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
